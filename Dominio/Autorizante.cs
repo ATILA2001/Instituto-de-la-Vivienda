a6a413076ab2468ca78587753c53ebac18f4c635
ATILA2001 <nahuel.carracedo@alumnos.frgp.utn.edu.ar>
2024-12-20 11:45:51 -0300

    autorizantes1

---
+++ Dominio/Autorizante.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Autorizante
+    {
+        public int Id { get; set; } // ID (clave primaria) de tipo INT
+        public Obra Obra { get; set; } // Obra (clave foránea) de tipo INT
+        public string CodigoAutorizante { get; set; } // Codigo del autorizante de tipo VARCHAR(50)
+        public string Detalle { get; set; } // Detalle de tipo VARCHAR(255)
+        public string Concepto { get; set; } // Concepto de tipo VARCHAR(255)
+        public EstadoAutorizante Estado { get; set; } // Estado (clave foránea) de tipo INT
+        public string Expediente { get; set; } // Expediente de tipo VARCHAR(50)
+        public decimal MontoAutorizado { get; set; } // Monto autorizado de tipo DECIMAL(15, 2)
+        public bool AutorizacionGG { get; set; } // Autorización GG (BIT) de tipo bool
+
+        public Autorizante() { }
+
+    }
+}
---
+++ Negocio/AutorizanteNegocio.cs
@@ -0,0 +1,220 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class AutorizanteNegocio
+    {
+        public List<Autorizante> listar(Usuario usuario)
+        {
+            List<Autorizante> lista = new List<Autorizante>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta(@"
+            SELECT 
+                CONCAT(C.NOMBRE, ' ', O.NUMERO, '/', O.AÑO) AS CONTRATA, 
+                O.DESCRIPCION AS OBRA, 
+                A.CODIGO_AUTORIZANTE, 
+                A.DETALLE, 
+                A.CONCEPTO, 
+                E.NOMBRE AS ESTADO, 
+                E.ID AS ESTADO_ID, 
+                A.EXPEDIENTE, 
+                A.MONTO_AUTORIZADO, 
+                A.AUTORIZACION_GG, 
+                AR.NOMBRE AS AREA, 
+                AR.ID AS AREA_ID, 
+                C.ID AS CONTRATA_ID
+            FROM 
+                AUTORIZANTES AS A
+            INNER JOIN 
+                OBRAS AS O ON A.OBRA = O.ID
+            INNER JOIN 
+                ESTADOS_AUTORIZANTES AS E ON A.ESTADO = E.ID
+            INNER JOIN 
+                CONTRATA AS C ON O.CONTRATA = C.ID
+            INNER JOIN 
+                BD_PROYECTOS AS B ON O.ID = B.ID_BASE
+            INNER JOIN 
+                AREAS AS AR ON O.AREA = AR.ID
+            WHERE 
+                O.AREA = @area");
+                datos.agregarParametro("@area", usuario.Area.Id);
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+
+                    Autorizante aux = new Autorizante();
+                    aux.CodigoAutorizante = datos.Lector["CODIGO_AUTORIZANTE"] as string;
+                    aux.Detalle = datos.Lector["DETALLE"] as string;
+                    aux.Concepto = datos.Lector["CONCEPTO"] as string;
+                    aux.Estado = new EstadoAutorizante
+                    {
+                        Nombre = datos.Lector["ESTADO"] as string,
+                        Id = (int)datos.Lector["ID"]
+                    };
+
+                    aux.Expediente = datos.Lector["EXPEDIENTE"] as string;
+                    aux.MontoAutorizado = (decimal)datos.Lector["MONTO_AUTORIZADO"];
+                    aux.AutorizacionGG = (bool)datos.Lector["AUTORIZACION_GG"];
+
+
+                    aux.Obra = new Obra
+                    {
+                        Descripcion = datos.Lector["OBRA"] as string,
+                        Contrata = new Contrata
+                        {
+                            Id = (int)datos.Lector["CONTRATA_ID"],  // Asignamos el ID de la Contrata
+                            Nombre = datos.Lector["CONTRATA"] as string  // Asignamos el nombre de la Contrata
+                        },
+
+                        // Asignamos el Area a la propiedad Obra
+                        Area = new Area
+                        {
+                            Id = (int)datos.Lector["AREA_ID"],  // Asignamos el ID del Area
+                            Nombre = datos.Lector["AREA"] as string  // Asignamos el nombre del Area
+                        }
+                    };
+
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public List<Obra> listar()
+        {
+            List<Obra> lista = new List<Obra>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta(@"
+            SELECT 
+                CONCAT(C.NOMBRE, ' ', O.NUMERO, '/', O.AÑO) AS CONTRATA, 
+                O.DESCRIPCION AS OBRA, 
+                A.CODIGO_AUTORIZANTE, 
+                A.DETALLE, 
+                A.CONCEPTO, 
+                E.NOMBRE AS ESTADO, 
+                E.ID AS ESTADO_ID, 
+                A.EXPEDIENTE, 
+                A.MONTO_AUTORIZADO, 
+                A.AUTORIZACION_GG, 
+                AR.NOMBRE AS AREA, 
+                AR.ID AS AREA_ID, 
+                C.ID AS CONTRATA_ID
+            FROM 
+                AUTORIZANTES AS A
+            INNER JOIN 
+                OBRAS AS O ON A.OBRA = O.ID
+            INNER JOIN 
+                ESTADOS_AUTORIZANTES AS E ON A.ESTADO = E.ID
+            INNER JOIN 
+                CONTRATA AS C ON O.CONTRATA = C.ID
+            INNER JOIN 
+                BD_PROYECTOS AS B ON O.ID = B.ID_BASE
+            INNER JOIN 
+                AREAS AS AR ON O.AREA = AR.ID");
+                datos.ejecutarLectura();
+                while (datos.Lector.Read())
+                {
+
+                    Autorizante aux = new Autorizante();
+                    aux.CodigoAutorizante = datos.Lector["CODIGO_AUTORIZANTE"] as string;
+                    aux.Detalle = datos.Lector["DETALLE"] as string;
+                    aux.Concepto = datos.Lector["CONCEPTO"] as string;
+                    aux.Estado = new EstadoAutorizante
+                    {
+                        Nombre = datos.Lector["ESTADO"] as string,
+                        Id = (int)datos.Lector["ID"]
+                    };
+
+                    aux.Expediente = datos.Lector["EXPEDIENTE"] as string;
+                    aux.MontoAutorizado = (decimal)datos.Lector["MONTO_AUTORIZADO"];
+                    aux.AutorizacionGG = (bool)datos.Lector["AUTORIZACION_GG"];
+
+
+                    aux.Obra = new Obra
+                    {
+                        Descripcion = datos.Lector["OBRA"] as string,
+                        Contrata = new Contrata
+                        {
+                            Id = (int)datos.Lector["CONTRATA_ID"],  // Asignamos el ID de la Contrata
+                            Nombre = datos.Lector["CONTRATA"] as string  // Asignamos el nombre de la Contrata
+                        },
+
+                        // Asignamos el Area a la propiedad Obra
+                        Area = new Area
+                        {
+                            Id = (int)datos.Lector["AREA_ID"],  // Asignamos el ID del Area
+                            Nombre = datos.Lector["AREA"] as string  // Asignamos el nombre del Area
+                        }
+                    };
+
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        /*
+        public bool agregar(Obra nuevaObra)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                // Consulta para insertar la nueva obra
+                datos.setearConsulta("INSERT INTO OBRAS (EMPRESA, AREA, CONTRATA, NUMERO, AÑO, ETAPA, OBRA, BARRIO, DESCRIPCION) " +
+                                     "VALUES (@EMPRESA, @AREA, @CONTRATA, @NUMERO, @AÑO, @ETAPA, @OBRA, @BARRIO, @DESCRIPCION)");
+
+                // Asignar los parámetros
+                datos.agregarParametro("@EMPRESA", nuevaObra.Empresa.Id);
+                datos.agregarParametro("@AREA", nuevaObra.Area.Id);
+                datos.agregarParametro("@CONTRATA", nuevaObra.Contrata.Id);
+                datos.agregarParametro("@NUMERO", nuevaObra.Numero);
+                datos.agregarParametro("@AÑO", nuevaObra.Año);
+                datos.agregarParametro("@ETAPA", nuevaObra.Etapa);
+                datos.agregarParametro("@OBRA", nuevaObra.ObraNumero);
+                datos.agregarParametro("@BARRIO", nuevaObra.Barrio.Id);
+                datos.agregarParametro("@DESCRIPCION", nuevaObra.Descripcion);
+
+                // Ejecutar la inserción
+                datos.ejecutarAccion();
+
+                // Si todo fue bien, devolvemos true
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // En caso de error, lanzamos la excepción para que se maneje donde se llame el método
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }*/
+    }
+}
