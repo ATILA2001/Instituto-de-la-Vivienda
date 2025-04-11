c14f7a67c7e2dc75d508c873aee9f11ac833da2b
ATILA2001 <nahuelcarracedo@gmail.com>
2025-04-11 16:45:40 -0300

    feat: New class for calculating redeterminations in a view

---
+++ Negocio/CalculoRedeterminacionNegocio.cs
@@ -0,0 +1,28 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class CalculoRedeterminacionNegocio
+    {
+        //public List<Array> listar()
+        //{
+        //    List<Autorizante> listaAut = new List<Autorizante>();
+        //    List<Certificado> listaCert = new List<Certificado>();
+        //    List<Redeterminacion> listaRedet = new List<Redeterminacion>();
+
+        //    AutorizanteNegocio autorizanteNegocio = new AutorizanteNegocio();
+        //    CertificadoNegocio certificadoNegocio = new CertificadoNegocio();
+        //    RedeterminacionNegocio redeterminacionNegocio = new RedeterminacionNegocio();
+
+        //    listaAut = autorizanteNegocio.listar();
+        //    listaCert = certificadoNegocio.listarFiltro();
+        //    listaRedet = redeterminacionNegocio.listar();
+
+        //}
+    }
+}
