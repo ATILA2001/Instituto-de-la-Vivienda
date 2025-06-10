d3bd3d95b004fda56d9593040b92131668813398
David <dev.dmartinoli@gmail.com>
2025-06-10 14:37:43 -0300

    refactor: remove unused using directives and clean up namespace in CalculoRedeterminacionNegocio

--- Negocio/CalculoRedeterminacionNegocio.cs
+++ Negocio/CalculoRedeterminacionNegocio.cs
@@ -2,19 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Negocio
-{
-    using Dominio;
-    using Dominio.Enums;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Security.Cryptography;
-    using System.Text;
-    using System.Threading.Tasks;
 
     namespace Negocio
     {
@@ -893,6 +880,5 @@ namespace Negocio
             }
 
 
-        }
     }
 }
