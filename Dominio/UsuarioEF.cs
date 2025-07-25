7f5e39c0483a9398c1010543b0b99b89b8e53be3
David <dev.dmartinoli@gmail.com>
2025-07-25 09:57:13 -0300

    feat: add properties for email, user type, and status in UsuarioEF class

--- Dominio/UsuarioEF.cs
+++ Dominio/UsuarioEF.cs
@@ -10,10 +10,15 @@ namespace Dominio
         [Key]
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public bool Tipo { get; set; } // true: Administrador, false: Usuario normal
+        public bool Estado { get; set; } // true: Activo, false: Inactivo
+
 
         [Column("AREA")]
         public int AreaId { get; set; }
         [ForeignKey("AreaId")]
         public virtual AreaEF Area { get; set; }
+
     }
 }
