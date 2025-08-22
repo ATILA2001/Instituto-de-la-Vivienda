91572a1d05d2e04a7402dc62408d2d7685572de4
David <dev.dmartinoli@gmail.com>
2025-08-22 10:22:36 -0300

    feat: update Legitimos link to point to the new LegitimosEF page

--- WebForms/Site.Master.designer.cs
+++ WebForms/Site.Master.designer.cs
@@ -69,15 +69,6 @@ namespace WebForms
         protected global::System.Web.UI.HtmlControls.HtmlAnchor lnkObras;
 
         /// <summary>
-        /// lnkLegitimos control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor lnkLegitimos;
-
-        /// <summary>
         /// liAdminNav control.
         /// </summary>
         /// <remarks>
