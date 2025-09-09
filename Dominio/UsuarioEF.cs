21666806509b9e512c6957f3ea693ed2a6035057
David <dev.dmartinoli@gmail.com>
2025-09-09 13:01:22 -0300

    feat: add CUIL and CONTRASENIA fields in UsuarioEF class

--- Dominio/UsuarioEF.cs
+++ Dominio/UsuarioEF.cs
@@ -11,9 +11,10 @@ namespace Dominio
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Correo { get; set; }
+        public string Contrasenia { get; set; }
         public bool Tipo { get; set; } // true: Administrador, false: Usuario normal
         public bool Estado { get; set; } // true: Activo, false: Inactivo
-
+        public string Cuil { get; set; }
 
         [Column("AREA")]
         public int? AreaId { get; set; }
